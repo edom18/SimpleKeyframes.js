d9a2b0c5360158089d7b16c677ce3c8c9a51ff6f
edom18 <edo.m18@gmail.com>
2013-01-26 00:34:41 +0900

    implemented Event emitter.

--- js/SimpleKeyframes.js
+++ js/SimpleKeyframes.js
@@ -121,6 +121,170 @@
         return ret;
     }
 
+
+    /**
+     * EvtEmit class.
+     * @constructor
+     * @example
+     * var targetObj = {};
+     * EvtEmit.attach(targetObj);
+     * //attach event
+     * targetObj.bind('update', function (data) {
+     *         alert('update!');
+     * });
+     * //dispatch event
+     * targetObj.trigger('update', data);
+     */
+    var EvtEmit = Class.extend({
+        trigger: function  (type, optData) {
+
+            var handlers,
+                handleArr,
+                l,
+                func;
+
+            if (!type) {
+                return false;
+            }
+
+            handlers = this.handlers || (this.handlers = {});
+            handleArr = handlers[type] || [];
+            l = handleArr.length;
+
+            while(l--) {
+                (func = handleArr[l]) &&
+                func[0].call(func[1] || this, optData);
+            }
+        },
+        bind: function (type, func, context) {
+            var handlers = this.handlers || (this.handlers = {});
+
+            if (!type) {
+                return false;
+            }
+
+            (handlers[type] || (handlers[type] = [])).push([func, context]);
+        },
+
+        one: function (type, func, context) {
+
+            var self = this;
+
+            function _func () {
+                self.off(type, _func);
+                func.apply(context, arguments);
+                context = null;
+                self  = null;
+                _func = null;
+            }
+
+            this.on(type, _func, context);
+        },
+
+        unbind: function (type, func) {
+    
+            var handlers,
+                handleArr,
+                i;
+
+            if (!type) {
+                return false;
+            }
+
+            handlers  = this.handlers || (this.handlers = {});
+            handleArr = handlers[type] || [];
+            i = handleArr.length;
+
+            if (!func) {
+                this.handlers[type] = [];
+            }
+            else {
+                while (i--) {
+                    handleArr[i][0] === func && handleArr.splice(i, 1);
+                }
+            }
+        }
+    });
+
+
+    /////////////////////////////////////////////////////////////////////
+    
+    //Shortcut.
+    EvtEmit.fn = EvtEmit.prototype;
+    EvtEmit.fn.fire = EvtEmit.fn.trigger;
+    EvtEmit.fn.on = EvtEmit.fn.bind;
+    EvtEmit.prototype.off = EvtEmit.fn.unbind;
+
+    /* ------------------------------------------------
+        Defined class method.
+    --------------------------------------------------- */
+    EvtEmit.attach = (function() {
+
+        var re = new RegExp('Object|Array|Date|Arguments', 'i');
+
+        /**
+         * util functions
+         */
+        function detectType(o) {
+            return Object.prototype.toString.call(o).replace(/^\[object (.+)\]$/, '$1');
+        }
+
+        function makeArray(args, sp) {
+            if (!sp) {
+                sp = 0;
+            }
+
+            return Array.prototype.slice.call(args, sp);
+        }
+
+        function _cloneObject(o, type) {
+
+            var newList = [],
+                i, e;
+
+            if(!type) {
+                type = detectType(o);
+            }
+            if (type == 'Array') {
+                for (i = 0; e = o[i];  i++) {
+                  newList.push(_cloneObject(e));
+                }
+
+                return newList;
+            }
+            else if (type == 'Object' || type == 'Arguments') {
+
+              return _extendObject({}, o, true);
+            }
+            else if (type == 'Date') {
+
+              return new Date(o.toString());
+            }
+            else {
+
+              return o;
+            }
+        }
+
+        function _extendObject(parent, obj, deepCopy) {
+
+            var val, type;
+
+            for (var key in obj) {
+                val = obj[key];
+                type = detectType(val);
+
+                parent[key] = (deepCopy && re.test(type)) ? _cloneObject(val, type) : val;
+            }
+
+            return parent;
+        }
+
+        return function(target) {
+            _extendObject(target, new EvtEmit, true);
+        };
+    })();
+
     ////////////////////////////////////////////////////////
 
     var Crono = Class.extend({
@@ -160,7 +324,6 @@
     });
 
     /////////////////////////////////////////////////////////////////
-
 
     var Stage = Crono.extend({
         init: function () {
@@ -208,9 +371,10 @@
 
     /////////////////////////////////////////////////////////////////
 
-    var Keyframes = Class.extend({
+    var Keyframes = EvtEmit.extend({
         init: function (frames) {
             this._frames = frames;
+            this._lastFrame = frames[frames.length - 1].frame;
         },
 
         /**
@@ -241,6 +405,10 @@
          */
         setParent: function (crono) {
             this._parent = crono;
+        },
+
+        getLastFrame: function () {
+            return this._lastFrame;
         },
 
         /**
@@ -309,6 +477,12 @@
                 }
             }
 
+            if (!to && from && from.frame <= pos) {
+                this.trigger('frameend', {
+                    lastFrame: from.frame
+                });
+            }
+
             return ret;
         }
     });
@@ -325,8 +499,10 @@
 
             keyframes = keyframes instanceof Keyframes ? Keyframes : new Keyframes(keyframes);
             keyframes.setParent(this);
+            keyframes.on('update', this._onUpdate, this);
 
             this._keyframes = keyframes;
+            this._lastFrame = keyframes.getLastFrame();
         },
         enterFrame: function (reverse) {
 
@@ -336,12 +512,19 @@
 
             var t  = reverse ? this._frame-- : this._frame++,
                 el = this.el,
+                keyframes = this._keyframes,
+                lastFrame = this._lastFrame,
                 props;
 
             if (t < 0) {
                 this._frame = t = 0;
             }
-            props = this._keyframes.getFrameAt(t);
+            else if (t > lastFrame) {
+                this._frame = t = lastFrame;
+                return;
+            }
+
+            props = keyframes.getFrameAt(t);
 
             for (var prop in props) {
                 el.style[prop] = props[prop];
@@ -357,6 +540,10 @@
         },
         go: function (pos) {
             this._frame = +pos;
+        },
+        _onUpdate: function (data) {
+            //this.stop();
+            //this._frame = data.lastFrame;
         }
     });
 
